fa184a394102479d2bf79c22640f1c5caef57ac1
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-04-17 09:21:39 -0300

    feat: Novos exercícios realizados.

---
+++ CsharpProjects/TestProject13/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] pallets = ["B14", "A11", "B12", "A13"];
+
+Array.Sort(pallets);
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"Sorted... : {pallet}");
+}
+
+Console.WriteLine("------------Reverse-----------");
+
+Array.Reverse(pallets);
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"Reverse... : {pallet}");
+}
