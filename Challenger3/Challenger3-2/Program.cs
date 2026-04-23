bccc606e532f0786cb678a807f887e629e1a50d0
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-04-23 14:38:02 -0300

    feat: Novo desafio envolvendo manipulação de matrizes

---
+++ Challenger3/Challenger3-2/Program.cs
@@ -0,0 +1,18 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] ids = orderStream.Split(',');
+
+Array.Sort(ids);
+
+foreach (string id in ids)
+{
+    if (id.Length == 4)
+    {
+        Console.WriteLine(id);
+    }
+    else
+    {
+        Console.WriteLine(id + "- Error");
+
+    }
+}
