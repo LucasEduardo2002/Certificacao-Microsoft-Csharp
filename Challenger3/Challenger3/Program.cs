907ee35a0863309fd0368c38c91d5f3dcecb3666
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-04-23 12:53:25 -0300

    feat: desafio 3 - métodos de manipulação de matrizes

---
+++ Challenger3/Challenger3/Program.cs
@@ -0,0 +1,12 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+char[] valuesArray = pangram.ToCharArray();
+Array.Reverse(valuesArray);
+
+string result = new string(valuesArray);
+
+string[] items = result.Split(' ');
+Array.Reverse(items);
+
+
+string result2 = String.Join(" ", items);
+Console.WriteLine(result2);
