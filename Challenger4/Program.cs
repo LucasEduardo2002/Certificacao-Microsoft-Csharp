16de300d98fca9af343d616291560f4d2d21d61a
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-04-24 18:07:34 -0300

    feat: Desafio para manipular cadeia de caractéries

---
+++ Challenger4/Program.cs
@@ -0,0 +1,21 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity;
+string output;
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = input.IndexOf(openSpan);
+int closingPosition = input.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+quantity = input.Substring(openingPosition, length);
+
+Console.WriteLine($"Quantity: {quantity}");
+
+output = input.Replace("<div>","");
+output = output.Replace("trade","reg");
+
+Console.WriteLine($"Output: {output}");
