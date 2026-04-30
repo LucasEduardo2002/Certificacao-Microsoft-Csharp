b54d822f8dc5fcbd9f3d5118706a35de374896cb
Lucas Eduardo <lucaseduardocn12@gmail.com>
2026-04-30 14:31:22 -0300

    feat: Conclusão do desafio para criação de método para exibir emails de funcionários de uma empresa

---
+++ Challenger5/Program.cs
@@ -0,0 +1,40 @@
+﻿string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+string internalDomain = "contoso.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    DisplayInternalEmail(corporate[i,0], corporate[i,1]);
+    // display internal email addresses
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    DisplayExternalEmail(external[i,0], external[i,1]);
+}
+
+void DisplayInternalEmail(string firstName, string lastName)
+{
+    string email = string.Concat(firstName.AsSpan(0,2), lastName);
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{internalDomain}");
+}
+
+void DisplayExternalEmail(string firstName, string lastName)
+{
+    string email = string.Concat(firstName.AsSpan(0,2), lastName);
+    email = email.ToLower();
+    Console.WriteLine($"{email}@{externalDomain}");
+}
